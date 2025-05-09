6ca8abd5d30fd6a27e384a3cd9d3ab886afeb9b2
lima79197@gmail.com <lima79197@gmail.com>
2025-05-09 18:05:40 -0300

    commit initial

---
+++ script.js
@@ -0,0 +1,31 @@
+const form = document.getElementById('input')
+let linhas = ''
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    addLinha()
+    atualizaTabela()
+})
+
+function addLinha(){
+    const inputNome = document.getElementById('nome-telefonico')
+    const inputTel = document.getElementById('numero-telefonico')
+
+
+    let linha = '<tr>'
+    linha += `<td>${inputNome.value}<td>`
+    linha += `<td>${inputTel.value}<td>`
+    linha += '<tr>'
+
+    linhas += linha
+
+    const corpoTabela = document.querySelector('tbody')
+    corpoTabela.innerHTML = linhas
+
+}
+
+function atualizaTabela(){
+    inputNome.value = ''
+    inputTel.value = ''
+}
